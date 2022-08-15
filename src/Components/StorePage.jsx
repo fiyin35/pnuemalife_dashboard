f8d2d56ea7f2db542e252dea362295e22c821009
Fiyinfolu Odeleke <fiyinfolu.odeleke@longbridgetech.com>
2022-08-15 15:58:26 +0100

    dashboard

--- src/Components/StorePage.jsx
+++ src/Components/StorePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './StorePages.css'
 import "./Responsiveness.css";
 import circle from '../assets/circle.png';
@@ -14,15 +14,81 @@ import power2 from '../assets/power2.png';
 import prayer1 from '../assets/prayer1.png';
 import prayer2 from '../assets/prayer2.png';
 
+import Box from '@mui/material/Box';
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import './modal.css';
+
+import BasicModal from './Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const StoreForm = () => {
+
+    const [formData, setFormData] = useState({
+        message: '',
+        title: ''
+    })
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+        <Button onClick={handleOpen}>Open modal</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style} >
+           <div className="modal_container"> 
+           <form className="form">
+                <div className="form__item">
+                    <TextField
+                        value={formData.message}
+                        id="fullName"
+                        label="Full name*"
+                        type="text"
+                        style={{ width: '100%', color: 'rgba(0,0,0,0.58)', backgroundColor: 'rgba(0, 0, 0, 0.05)', borderRadius: '5px' }}
+                        variant="filled"
+                        onChange={(e)=>setFormData({...formData, message: e.target.value})}
+                    />
+                </div>
+            </form>
+           </div>
+          </Box>
+        </Modal>
+      </div>
+    )
+}
 
 
 function StorePage() {
+
+    const [openStoreForm, setOpenStoreForm] = useState(false)
+
+    const toggleStore = ()=>setOpenStoreForm(!openStoreForm);
+
     return (
         <div className="container">
             <div className="headnav">
                 <div className="title">
                     <h2>Store</h2>
-                    <img src={circle} alt="" />
+                    <img src={circle} alt="" className="circle" onClick={toggleStore} />
                 </div>
                 <div className="profile">
                     <div className="search">
@@ -81,7 +147,7 @@ function StorePage() {
                         </div>
                     </div>
                 </div>
-                <div className="books">
+                {/* <div className="books">
                     <h2>Books on spirit</h2>
                     <div className="slide">
                         <div className="spirit">
@@ -151,8 +217,8 @@ function StorePage() {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="books">
+                </div> */}
+                {/* <div className="books">
                     <h2>Books on Prayer</h2>
                     <div className="slide">
                         <div className="spirit">
@@ -189,7 +255,7 @@ function StorePage() {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
